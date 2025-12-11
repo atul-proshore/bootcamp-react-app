b9abd3515086a8f5b0acc474f257fbf59fb8a2ee
aashika262 <147691685+aashika262@users.noreply.github.com>
2025-12-11 16:49:54 +0545

    validation in login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,39 +6,8 @@ import InputGroupComp from "@/components/reusableComponents/InputGroupComp";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-6 justify-center items-center mt-10">
+    <div className="flex flex-col gap-6 items-center mt-10">
       <Login />
-      <ButtonComp name="This is a button" />
-      <CardComp className="flex justify-center items-center">
-        <p className="font-semibold ">This is header</p>
-        This is card body
-      </CardComp>
-      {/* <InputComp
-        className=""
-        // label="label"
-        placeholder="placeholder text"
-        // validationMessage="this is validation text"
-      />
-      <InputComp
-        className=""
-        placeholder="placeholder text"
-        validationMessage="this is validation text"
-      /> */}
-      <InputGroupComp
-        label="Password"
-        // startContent={<EyeIcon />}
-        // endContent={<EyeIcon />}
-        type="password"
-        borderColor="blue"
-        placeholder="placeholder text"
-        validationMessage="this is validation text"
-      />
-      <InputGroupComp
-        type="text"
-        borderColor="green"
-        placeholder="placeholder text"
-        validationMessage="this is validation text"
-      />
     </div>
   );
 }
--- src/components/reusableComponents/InputGroupComp.tsx
+++ src/components/reusableComponents/InputGroupComp.tsx
@@ -17,6 +17,7 @@ interface IInputProps {
   className?: string;
   validationMessage?: string;
   wrapperClassName?: string;
+  props?: any;
 }
 
 const InputGroupComp = ({
@@ -30,6 +31,7 @@ const InputGroupComp = ({
   className,
   validationMessage,
   wrapperClassName,
+  props,
 }: IInputProps) => {
   return (
     <div className={`relative ${wrapperClassName}`}>
@@ -45,6 +47,7 @@ const InputGroupComp = ({
               ? "focus:border-[#97BE0D] focus:ring-2 focus:ring-[#97BE0D]/60 focus:outline-none"
               : "focus:border-[#006FB7] focus:ring-2 focus:ring-[#006FB7]/60 focus:outline-none"
           } ${className}`}
+          {...props}
           placeholder={placeholder}
         />
         {endContent && (
