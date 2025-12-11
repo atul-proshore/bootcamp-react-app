e5bc4f8e181698b72cba06b4005ef9c8b64bf8b1
aashika262 <147691685+aashika262@users.noreply.github.com>
2025-12-11 13:26:40 +0545

    login UI done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,41 @@
-export default function page() {
-  return <div>page</div>;
+import ButtonComp from "@/components/reusableComponents/ButtonComp";
+import CardComp from "@/components/reusableComponents/CardComp";
+import InputComp from "@/components/reusableComponents/InputComp";
+import { Button } from "@/components/shadcnUI/button";
+import { Checkbox } from "@/components/shadcnUI/checkbox";
+
+export default function Login() {
+  return (
+    <CardComp className="flex flex-col gap-6 p-6">
+      <div className="flex flex-col justify-center items-center">
+        <p className="font-semibold text-2xl text-[#006FB7] ">Welcome Back!</p>
+        <p className="text-[#1E1E1E]">Sign in to your account</p>
+      </div>
+      <div className="flex flex-col gap-4 justify-center items-center m-4">
+        <InputComp
+          label="Username"
+          placeholder="Enter your Username"
+          // validationMessage="Email is required"
+          className="w-full"
+          wrapperClassName="w-full max-w-sm"
+        />
+        <InputComp
+          label="Password"
+          placeholder="Enter Password"
+          // validationMessage="Password is required"
+          className="w-full"
+          wrapperClassName="w-full max-w-sm"
+        />
+      </div>
+      <div className="flex justify-center items-center gap-2 ">
+        <Checkbox id="remember-me" />
+        <label htmlFor="remember-me" className=" text-sm text-gray-600">
+          Remember me
+        </label>
+      </div>
+      <div className="flex justify-center">
+        <ButtonComp name="Sign In" disabled={false} btnColor="green" />
+      </div>
+    </CardComp>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ButtonComp from "@/components/reusableComponents/ButtonComp";
 import CardComp from "@/components/reusableComponents/CardComp";
 import InputComp from "@/components/reusableComponents/InputComp";
+import Login from "./login/page";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 justify-center items-center mt-10">
+      <Login />
       <ButtonComp name="This is a button" />
       <CardComp className="flex justify-center items-center">
         <p className="font-semibold ">This is header</p>
