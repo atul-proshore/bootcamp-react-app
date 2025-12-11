5f455b225f14ca365bb134b1aa68a9972c528e2e
bikki10 <bikrambhusal75@gmail.com>
2025-12-11 13:23:43 +0545

    Input group change

---
+++ src/app/utils/utils.ts
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+export const loginSchema = yup.object({
+  email: yup.string().required("Fullname is required!"),
+  password: yup.string().required("Password is required!"),
+});
+
+export const registerSchema = yup.object().shape({
+  fullName: yup.string().required("Full name is required"),
+  username: yup.string().required("Username is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup.string().min(8, "Must be at least 8 characters").required(),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Passwords must match")
+    .required(),
+});
