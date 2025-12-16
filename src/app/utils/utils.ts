ffca8f511ed2b54604668ae9ff84123eeced23df
bikki10 <bikrambhusal75@gmail.com>
2025-12-16 20:34:01 +0545

    Merge pull request #12 from atul-proshore/feature/bikram-components
    Feature/bikram components

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,7 +1,11 @@
 import * as yup from "yup";
 
 export const loginSchema = yup.object({
-  email: yup.string().email("Invalid email").required("Email is required!"),
+  email: yup
+    .string()
+    .email("Invalid email")
+    .required("Email is required!")
+    .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Email must contain @ and ."),
   password: yup.string().required("Password is required!"),
 });
 
@@ -9,13 +13,10 @@ export const registerSchema = yup.object().shape({
   fullName: yup.string().required("Full name is required"),
   username: yup.string().required("Username is required"),
   email: yup
-  .string()
-  .email("Invalid email")
-  .required("Email is required!")
-  .matches(
-    /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-    "Email must contain @ and ."
-  ),
+    .string()
+    .email("Invalid email")
+    .required("Email is required!")
+    .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Email must contain @ and ."),
   password: yup.string().min(8, "Must be at least 8 characters").required(),
   confirmPassword: yup
     .string()
